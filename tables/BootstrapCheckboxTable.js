093ccf001367d01457e5de5ec59b44c0b05a6466
Blaise Iradukunda <iradub0@sewanee.edu>
2019-01-22 11:03:23 -0800

    table with checkbox at the end

---
+++ tables/BootstrapCheckboxTable.js
@@ -0,0 +1,41 @@
+export default class ArBootrapTableCheckable extends Component{
+    constructor(props){
+        super(props)
+        this.state ={
+            selectedRow: -1
+        }
+    }
+    generateRow(row){
+        return row.map(
+            (rowData, i) =><td key={i}>{rowData}</td>
+        )
+    }
+    render(){
+        const data = this.props.data? this.props.data : {}
+        const headers = data.headers? data.headers: []
+        const rows = data.rows? data.rows : []
+        return (
+            <Table striped bordered hover size="sm">
+                <thead>
+                    {
+                        headers.map(
+                            (header, i)=><th key='i'>{header}</th>
+                        )
+                    }
+                </thead>
+                <tbody>
+                    {
+                        rows.map(
+                            (row, i) =>(
+                                <tr key={i}>
+                                    {this.generateRow(row)}
+                                    <td><Form.Check /></td>
+                                </tr>
+                            )
+                        )
+                    }
+                </tbody>
+            </Table>
+        )
+    }
+}
